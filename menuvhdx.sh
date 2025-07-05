a0d00bed1b02e1e02860e57853ec84ee672d3147
SJCreatividad <samueljaimesofficial@gmail.com>
2025-07-05 16:15:49 +0200

    Update scripts - personal revision code

--- menuvhdx.sh
+++ menuvhdx.sh
@@ -1,22 +1,25 @@
-read -p "Ingrese el nombre del archivo VHDX: " vhdx_file
+sudo modprobe nbd max_part=8
+read -p "Ingrese el nombre del archivo VHDX (sin extensión): " vhdx_file
 read -p "Ingrese el numero del disco: " disk_number
-vhdx_file="$vhdx_file.vhdx"
-disk_number="nbd$disk_number"
+read -p "Ingrese su usuario y medio de montaje (ejemplo: `usuario/USB`): " mount_point
+vhdx_disk="$vhdx_file.vhdx"
+device_number="nbd$disk_number"
 
-if [ -f "/media/canelo/SD/$vhdx_file" ]; then
+if [ -f "/media/$mount_point/$vhdx_disk" ]; then
     echo "El archivo VHDX existe."
 else
     echo "El archivo VHDX no existe."
     exit 1
 fi
 
-echo "Montando el archivo VHDX..."
-sudo modprobe nbd max_part=8
-sudo qemu-nbd --connect=/dev/$disk_number /media/canelo/SD/"$vhdx_file"
-sudo partprobe /dev/$disk_number
-sudo mkdir /mnt/vhdx_mount/"$vhdx_file"
-sudo mount /dev/$disk_numberp2 /mnt/vhdx_mount/"$vhdx_file"
+sudo qemu-nbd --connect=/dev/$device_number /media/"$mount_point"/"$vhdx_disk"
+sudo partprobe /dev/$device_number
+lsblk
+read -p "Ingrese el numero de la particion que desea montar (ejemplo: 1, 2, etc.): " number_partition
+sudo mkdir -p /mnt/vhdx_mount/"$vhdx_file"
+sudo mount /dev/${device_number}p$number_partition /mnt/vhdx_mount/"$vhdx_file"
 
+echo "Montando el archivo VHDX..."
 echo "El archivo VHDX se ha montado correctamente."
 echo "El punto de montaje es /mnt/vhdx_mount/$vhdx_file"
 echo "Para desmontar el archivo VHDX, ejecute el script unmount.sh"
--- unmount.sh
+++ unmount.sh
@@ -1,10 +1,10 @@
 read -p "Ingrese el nombre del archivo VHDX: " vhdx_file
-vhdx_file="$vhdx_file.vhdx"
+read -p "Ingrese el numero de disco: " disk_number
 
 sudo umount /mnt/vhdx_mount/"$vhdx_file"
-sudo qemu-nbd --disconnect /dev/nbd0
+sudo qemu-nbd --disconnect /dev/nbd$disk_number
 sudo rm -rf /mnt/vhdx_mount/"$vhdx_file"
-sudo rm -rf /dev/nbd0
-sudo rm -rf /dev/nbd0p2
-sudo rm -rf /dev/nbd0p1
-sudo rm -rf /dev/nbd0p0
+sudo rm -rf /dev/nbd$disk_number
+sudo rm -rf /dev/nbd${disk_number}p2
+sudo rm -rf /dev/nbd${disk_number}p1
+sudo rm -rf /dev/nbd${disk_number}p0
